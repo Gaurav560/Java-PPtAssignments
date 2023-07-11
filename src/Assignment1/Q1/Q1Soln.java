1413d4c09dae66498319a39f9def4ec782992804
GAURAV SHARMA <gsharma1154@gmail.com>
2023-07-11 18:10:33 +0530

    1st assignment q1 soln

---
+++ src/Assignment1/Q1/Q1Soln.java
@@ -0,0 +1,9 @@
+package Assignment1.Q1;
+
+public class Q1Soln {
+    //compiler-it is a software which scans the whole high level code and converts it into machine level code .
+    //where as interpreter scans the code line by ine and then converts it into machine level code .
+    //java is both compiled and interpreted language
+    //where as python,javascript is interpreted language
+    //c,c#,c++ are compiled language 
+}
